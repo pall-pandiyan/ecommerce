356b8462cf8fe213056deaa5f3e154b1a539fe10
Pall Pandiyan.S <pallpandiyan@gmail.com>
2023-08-11 23:17:12 +0530

    defined a base product model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductModel(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField(null=True, blank=True)
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
