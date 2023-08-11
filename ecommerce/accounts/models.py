7fbb6e9c92becbdf9683166a0aad754483826822
Pall Pandiyan.S <pallpandiyan@gmail.com>
2023-08-11 23:08:29 +0530

    defined a simple user profile model

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class UserProfileModel(models.Model):
+    """
+    Custom user model with custom fields.
+    Developer: Pall Pandiyan.S
+    """
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
